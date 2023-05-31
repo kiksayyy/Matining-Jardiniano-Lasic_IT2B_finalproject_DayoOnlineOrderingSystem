53d1703c629e9a220a84614070d0fc93c232feef
MatiningJeleane <matiningjeleane@gmail.com>
2023-05-31 23:09:15 +0800

    loginForm

---
+++ loginform.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration Form</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+<div class="start">
+
+    <h2>Create Account</h2>
+    <form method="post" action="register.php">
+        <div class="signins">
+      
+              <div class="signin">
+                <input type="text" name='username' id='username'required>
+                <span></span>
+                <label>Username</label>
+              </div>
+            
+              <div class="signin">
+                <input type="password" name='password'id='password'required>
+                <span></span>
+                <label>Password</label>
+              </div>
+            
+        <input type="submit" value="Register">
+
+        </div>
+    </form>
+</div>
